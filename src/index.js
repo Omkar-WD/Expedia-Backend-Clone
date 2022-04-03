3dea7316430e1093f14e5c19cbf9fa75bc702226
prathamsm7 <79472183+prathamsm7@users.noreply.github.com>
2022-04-03 18:33:03 +0530

    payment code updated

--- src/index.js
+++ src/index.js
@@ -1,24 +1,26 @@
-require("dotenv").config({ path: "../.env" });
+require('dotenv').config({ path: '../.env' });
 // require("dotenv").config();
-const cors = require("cors");
+const cors = require('cors');
 
-const express = require("express");
+const express = require('express');
 const app = express();
-const connect = require("./config/db");
+const connect = require('./config/db');
 
 // Routes
-const authController = require("./controllers/auth.controller");
-const userController = require("./controllers/user.controller");
-const favouriteController = require("./controllers/favourite.controller");
-const hotelController = require("./controllers/hotel.controller");
+const authController = require('./controllers/auth.controller');
+const userController = require('./controllers/user.controller');
+const favouriteController = require('./controllers/favourite.controller');
+const hotelController = require('./controllers/hotel.controller');
+const paymentController = require('./controllers/razorPay.controller');
 
 app.use(express.json());
 app.use(cors());
 
-app.use("/auth", authController);
-app.use("/hotel", hotelController);
-app.use("/favourite", favouriteController);
-app.use("/user", userController);
+app.use('/auth', authController);
+app.use('/hotel', hotelController);
+app.use('/favourite', favouriteController);
+app.use('/user', userController);
+app.use('/payment', paymentController);
 
 const PORT = process.env.PORT;
 // Db Connection
---
+++ src/models/payment.model.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const paymentSchema = new mongoose.Schema(
+  {
+    hotelId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'hotel',
+      required: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'user',
+      required: true,
+    },
+    rooms: { type: String },
+    night: { type: String },
+    checkinDate: { type: String },
+    checkoutDate: { type: String },
+    transactionId: { type: String },
+    amount: { type: String },
+    bookingDate: { type: String },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model('payment', paymentSchema);
